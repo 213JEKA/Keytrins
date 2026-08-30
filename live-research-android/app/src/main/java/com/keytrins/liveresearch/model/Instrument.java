cff27187eb25fa8c2062f561763118541f246184
213Jeka <123963350+213JEKA@users.noreply.github.com>
2026-08-30 03:57:58 +0300

    Add instrument model

---
+++ live-research-android/app/src/main/java/com/keytrins/liveresearch/model/Instrument.java
@@ -0,0 +1,21 @@
+package com.keytrins.liveresearch.model;
+
+import java.math.BigDecimal;
+
+public final class Instrument {
+    public final String symbol;
+    public final String baseCoin;
+    public final BigDecimal tickSize, qtyStep, minQty, maxMarketQty, minNotional;
+    public double turnover24h;
+
+    public Instrument(String symbol, String baseCoin, BigDecimal tickSize, BigDecimal qtyStep,
+                      BigDecimal minQty, BigDecimal maxMarketQty, BigDecimal minNotional) {
+        this.symbol = symbol;
+        this.baseCoin = baseCoin;
+        this.tickSize = tickSize;
+        this.qtyStep = qtyStep;
+        this.minQty = minQty;
+        this.maxMarketQty = maxMarketQty;
+        this.minNotional = minNotional;
+    }
+}
